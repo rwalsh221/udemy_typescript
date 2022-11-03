025312acb3ea12736a36118d0e1e870f6fa9f370
Richard Walsh <richwalsh2@googlemail.com>
2022-11-03 20:34:12 +0000

    section 2 lesson 1

---
+++ section_1/lesson_4/script.js
@@ -0,0 +1,13 @@
+// const button = document.querySelector('button');
+// const input1 = document.querySelector('#num1');
+// const input2 = document.querySelector('#num2');
+
+// console.log(input1);
+
+// const add = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// button.addEventListener('click', () => {
+//   console.log(add(input1.value, input2.value));
+// });
