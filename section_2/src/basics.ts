f257273dd43b3a0619dca898916c366d1147381a
Richard Walsh <richwalsh2@googlemail.com>
2022-11-05 21:06:46 +0000

    section 2 lesson 16

---
+++ section_2/src/basics.ts
@@ -0,0 +1,17 @@
+const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
+  if (showResult) {
+    return console.log(`${phrase} ${n1 + n2}`);
+  } else {
+    return n1 + n2;
+  }
+};
+
+const number1 = 5;
+const number2 = 2.8;
+
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);
+
+export {};
--- section_2/src/main.ts
+++ section_2/src/main.ts
@@ -1,12 +1,10 @@
-const add = (n1: number, n2: number) => {
-  return n1 + n2;
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Richard',
+  age: 37,
 };
 
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
-
-console.log(result);
-
+console.log(person.name);
 export {};
