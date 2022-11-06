d41ad91a82c8d299095f3f8daa0da918d0941623
Richard Walsh <richwalsh2@googlemail.com>
2022-11-06 21:18:47 +0000

    section 2 lesson 25

---
+++ section_2/src/obj.ts
@@ -0,0 +1,12 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: 'Richard',
+  age: 30,
+  hobbies: ['sports', 'cooking'],
+};
+
+console.log(person.name);
+export {};
