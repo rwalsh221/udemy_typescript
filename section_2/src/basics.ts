5c6d796590ba916a3cba8222327ffeb9d7a48006
Richard Walsh <richwalsh2@googlemail.com>
2023-04-11 21:29:35 +0100

    s2 lesson 17

--- section_2/src/basics.ts
+++ section_2/src/basics.ts
@@ -1,17 +1,22 @@
-const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
-  if (showResult) {
-    return console.log(`${phrase} ${n1 + n2}`);
-  } else {
-    return n1 + n2;
-  }
-};
-
-const number1 = 5;
-const number2 = 2.8;
-
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
-
-export {};
+const add = (
+    n1: number,
+    n2: number,
+    showResult: boolean,
+    resultPhrase: string
+  ) => {
+    if (showResult) {
+      return console.log(`${resultPhrase} ${n1 + n2}`);
+    } else {
+      return n1 + n2;
+    }
+  };
+  
+  const number1 = 5;
+  const number2 = 2.5;
+  const printResult = true;
+  const phrase = 'the result is';
+  
+  const result = add(number1, number2, printResult, phrase);
+  
+  console.log(result);
+  
