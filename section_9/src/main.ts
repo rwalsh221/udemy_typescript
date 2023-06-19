91b3161bb949eedef091ac99d6444e80ffd92bfb
Richard Walsh <richwalsh2@googlemail.com>
2023-06-19 19:37:02 +0100

    section 9 lesson 124

---
+++ section_9/src/main.ts
@@ -0,0 +1,70 @@
+// autobind decorator
+
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// PROECTINPUT CLASS
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  test: String;
+
+  constructor() {
+    this.test = 'hello';
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = 'user-input';
+
+    this.titleInputElement = this.formElement.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
+  }
+
+  private configure() {
+    this.formElement.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+  }
+}
+
+const projInput = new ProjectInput();
